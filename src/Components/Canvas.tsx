b6bfaa41d2246f077764e1733fbdd19f8709aaf2
Vinícius Regatieri <viniciusvcr@hotmail.com>
2020-05-04 01:56:11 -0300

    english names

--- src/Components/Canvas.tsx
+++ src/Components/Canvas.tsx
@@ -23,85 +23,93 @@ export default function Canvas() {
 
   return (
     <div className="container">
-      <div className="cointainer" id="center">
-        <div className="row align-items-start">
-          <div className="col m-1" id="conteudo-left">
-            <div style={{ textAlign: "center" }}>
-              <div style={{ paddingTop: "8px", paddingBottom: "8px" }}>
-                <Button
-                  variant="outline-light"
-                  className="btn btn-outline-dark btn-sm m-1"
-                  id="lineButton"
-                >
-                  Linha (l)
-                </Button>
-                <Button
-                  variant="outline-light"
-                  className="btn btn-outline-dark btn-sm m-1"
-                  id="rectButton"
-                >
-                  Retângulo (r)
-                </Button>
-                <Button
-                  variant="outline-light"
-                  className="btn btn-outline-dark btn-sm m-1"
-                  id="triangleButton"
-                >
-                  Triângulo (t)
-                </Button>
-                <Button
-                  variant="outline-light"
-                  className="btn btn-outline-dark btn-sm m-1"
-                  id="circleButton"
-                >
-                  Círculo (c)
-                </Button>
-              </div>
-              <div>
-                <span
-                  className="badge badge-light"
-                  style={{ fontSize: "14px" }}
-                  id="currentTask"
-                >
-                  Selecione acima a ferramenta a ser usada
-                </span>
-              </div>
-              <div style={{ paddingTop: "8px" }}>
-                <canvas
-                  id="canvas"
-                  width={1024}
-                  height={768}
-                  style={{
-                    border: "1px solid black",
-                    boxShadow: "0px 2px 10px -5px rgba(0,0,0,0.75)",
-                  }}
-                  onMouseMove={(e) => {
-                    dispatch(getAxis(e, store));
-                  }}
-                ></canvas>
+      <div className="row align-items-start">
+        <div
+          className="cointainer"
+          id="center"
+          style={{ margin: "0 auto", textAlign: "center" }}
+        >
+          <div className="row align-items-start">
+            <div className="col m-1 box" id="conteudo-left">
+              <div style={{ textAlign: "center" }}>
+                <div style={{ paddingTop: "8px", paddingBottom: "8px" }}>
+                  <Button
+                    variant="outline-light"
+                    className="btn btn-outline-dark btn-sm m-1"
+                    id="lineButton"
+                  >
+                    Line (l)
+                  </Button>
+                  <Button
+                    variant="outline-light"
+                    className="btn btn-outline-dark btn-sm m-1"
+                    id="rectButton"
+                  >
+                    Rectangle (r)
+                  </Button>
+                  <Button
+                    variant="outline-light"
+                    className="btn btn-outline-dark btn-sm m-1"
+                    id="triangleButton"
+                  >
+                    Triangle (t)
+                  </Button>
+                  <Button
+                    variant="outline-light"
+                    className="btn btn-outline-dark btn-sm m-1"
+                    id="circleButton"
+                  >
+                    Circle (c)
+                  </Button>
+                </div>
                 <div>
-                  <Button
-                    className="btn btn-outline-dark btn-sm m-1"
-                    variant="outline-light"
-                    id="xAxis"
-                    style={{ paddingLeft: "8px" }}
+                  <span
+                    className="badge badge-light"
+                    style={{ fontSize: "14px" }}
+                    id="currentTask"
                   >
-                    Eixo X: {position?.x}
-                  </Button>
-                  <Button
-                    className="btn btn-outline-dark btn-sm m-1"
-                    variant="outline-light"
-                    id="yAxis"
-                    style={{ paddingLeft: "8px" }}
-                  >
-                    Eixo Y: {position?.y}
-                  </Button>
+                    Select a tool
+                  </span>
+                </div>
+                <div style={{ paddingTop: "8px" }}>
+                  <canvas
+                    id="canvas"
+                    width={1000}
+                    height={744}
+                    style={{
+                      border: "1px solid black",
+                      boxShadow: "0px 2px 10px -5px rgba(0,0,0,0.75)",
+                    }}
+                    onMouseMove={(e) => {
+                      dispatch(getAxis(e, store));
+                    }}
+                  ></canvas>
+                  <div>
+                    <span
+                      className="badge badge-dark m-1"
+                      id="xAxis"
+                      style={{ paddingLeft: "8px" }}
+                    >
+                      Axis X: {position?.x}
+                    </span>
+                    <span
+                      className="badge badge-dark m-1"
+                      id="yAxis"
+                      style={{ paddingLeft: "8px" }}
+                    >
+                      Axis Y: {position?.y}
+                    </span>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="col m-1" id="conteudo-right">
-            <ObjectList />
+            <div
+              className="col m-1 box"
+              id="conteudo-right"
+              style={{ paddingTop: "79px" }}
+            >
+              <ObjectList />
+            </div>
           </div>
         </div>
       </div>
--- src/Components/ObjectList.tsx
+++ src/Components/ObjectList.tsx
@@ -1,7 +1,15 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 
 export default class ObjectList extends Component {
   render() {
-    return <div></div>;
+    return (
+      <Fragment>
+        <table className="table table-sm table-hover" id="object-table">
+          <tr>
+            <th>Objetos na tela</th>
+          </tr>
+        </table>
+      </Fragment>
+    );
   }
 }
