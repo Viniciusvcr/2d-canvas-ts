58eac42ddaa99aed20d30b628ffa9af56af19f4b
Vinícius Regatieri <viniciusvcr@hotmail.com>
2020-05-11 00:09:18 -0300

    all Tools working

--- src/Components/Tools.tsx
+++ src/Components/Tools.tsx
@@ -11,6 +11,15 @@ interface Props {
 const Tools: React.FC<Props> = (props: Props) => {
   const { mouseDispatcher, mouseStore } = props;
 
+  function handleClick(createFn: Function, pointsRequired: number) {
+    mouseDispatcher({
+      type: "INIT_DRAWING",
+      mousePoint: mouseStore.position,
+      createFn,
+      pointsRequired,
+    });
+  }
+
   const style: React.CSSProperties = {
     alignSelf: "center",
   };
@@ -22,12 +31,7 @@ const Tools: React.FC<Props> = (props: Props) => {
         className="btn btn-outline-dark btn-sm m-1"
         id="lineButton"
         onClick={() => {
-          mouseDispatcher({
-            type: "INIT_DRAWING",
-            mousePoint: mouseStore.position,
-            createFn: ShapeController.createLine,
-            pointsRequired: 2,
-          });
+          handleClick(ShapeController.createLine, 2);
         }}
       >
         Line (l)
@@ -36,6 +40,9 @@ const Tools: React.FC<Props> = (props: Props) => {
         variant="outline-light"
         className="btn btn-outline-dark btn-sm m-1"
         id="rectButton"
+        onClick={() => {
+          handleClick(ShapeController.createRectangle, 2);
+        }}
       >
         Rectangle (r)
       </Button>
@@ -43,6 +50,9 @@ const Tools: React.FC<Props> = (props: Props) => {
         variant="outline-light"
         className="btn btn-outline-dark btn-sm m-1"
         id="triangleButton"
+        onClick={() => {
+          handleClick(ShapeController.createTriangle, 3);
+        }}
       >
         Triangle (t)
       </Button>
@@ -50,6 +60,9 @@ const Tools: React.FC<Props> = (props: Props) => {
         variant="outline-light"
         className="btn btn-outline-dark btn-sm m-1"
         id="circleButton"
+        onClick={() => {
+          handleClick(ShapeController.createCircle, 2);
+        }}
       >
         Circle (c)
       </Button>
